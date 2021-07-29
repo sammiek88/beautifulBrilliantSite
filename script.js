7199baa5e1f399e33cdc9bbd41c3d2c4a6c4cf36
sammiek88 <67045522+sammiek88@users.noreply.github.com>
2021-07-29 15:17:54 +0100

    consent banner

--- script.js
+++ script.js
@@ -21,8 +21,45 @@
 
 // List<Person> people = response.getPeople();
 
-// twitter notification after click
+//cookie consent banner//
 
+const cookieStorage = {
+  getItem: (key) => {
+    const cookies = document.cookie
+      .split(';')
+      .map((cookie) => cookie.split('='))
+      .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+    return cookies[key];
+  },
+  setItem: (key, value) => {
+    document.cookie = `${key}=${value}`;
+  },
+};
+
+const storageType = cookieStorage;
+const consentPropertyName = 'BB_consent';
+
+const shouldShowPopUp = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+  const consentPopup = document.getElementById('consentpopup');
+  const acceptBtn = document.getElementById('accept');
+
+  const acceptFn = (event) => {
+    saveToStorage(storageType);
+    consentPopup.classList.add('hidden');
+  };
+  acceptBtn.addEventListener('click'.acceptFn);
+
+  if (shouldShowPopUp(storageType)) {
+    setTimeout(() => {
+      consentPopup.classList.remove('hidden');
+    }, 2000);
+  }
+};
+
+// twitter notification after click
 function twitter() {
   alert(
     `Ooops! We don't have a twitter account yet! Watch this space however its in development! In the meantime you can find us on facebook here: "https://www.facebook.com/alwaysbeBB" `
