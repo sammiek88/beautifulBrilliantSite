df951a78e9e29665bffb2b2f4c938b9e465b295f
sammiek88 <67045522+sammiek88@users.noreply.github.com>
2021-07-29 15:56:36 +0100

    Cookie banner

--- script.js
+++ script.js
@@ -23,39 +23,19 @@
 
 //cookie consent banner//
 
-const cookieStorage = {
-  getItem: (item) => {
-    const cookies = document.cookie
-      .split(';')
-      .map((cookie) => cookie.split('='))
-      .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
-    return cookies[item];
-  },
-  setItem: (item, value) => {
-    document.cookie = `${item}=${value};`;
-  },
-};
+const cookieContainer = document.querySelector('.cookie-container');
+const cookieButton = document.querySelector('.cookie-btn');
 
-const storageType = cookieStorage;
-const consentPropertyName = 'jdc_consent';
-const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
-const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+cookieButton.addEventListener('click', () => {
+  cookieContainer.classList.remove('active');
+  localStorage.setItem('cookieBannerDisplayed', 'true');
+});
 
-window.onload = () => {
-  const acceptFn = (event) => {
-    saveToStorage(storageType);
-    consentPopup.classList.add('hidden');
-  };
-  const consentPopup = document.getElementById('consent-popup');
-  const acceptBtn = document.getElementById('accept');
-  acceptBtn.addEventListener('click', acceptFn);
-
-  if (shouldShowPopup(storageType)) {
-    setTimeout(() => {
-      consentPopup.classList.remove('hidden');
-    }, 2000);
+setTimeout(() => {
+  if (!localStorage.getItem('cookieBannerDisplayed')) {
+    cookieContainer.classList.add('active');
   }
-};
+}, 2000);
 
 // twitter notification after click
 function twitter() {
